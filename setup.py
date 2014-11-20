626bc4f1f8be9b75215278d90b34bf041069cc02
Russell Stewart <russell.s.stewart@gmail.com>
2014-11-19 21:17:27 -0800

    Added official cython pip dependency.

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ import sys
 
 setup(
     name='runcython',
-    version='0.1.7',
+    version='0.1.8',
     description='compile and run cython in one line',
     license='MIT',
     url='https://github.com/Russell91/runcython',
     long_description='https://github.com/Russell91/runcython',
+    install_requires='cython',
     scripts = ['runcython', 'runcython++', 'makecython', 'makecython++'],
 )
