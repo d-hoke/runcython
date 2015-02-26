4a359ff764aa0c5348a82131b8e4e0373415b296
Russell Stewart <russell.s.stewart@gmail.com>
2015-02-25 22:04:24 -0800

    0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 setup(
     name='runcython',
-    version='0.2.0',
+    version='0.2.1',
     description='compile and run cython in one line',
     license='MIT',
     url='https://github.com/Russell91/runcython',
