c818a91c8624dae40ed27fbff10ad1eb415fa42a
Russell Stewart <russell.s.stewart@gmail.com>
2016-05-02 13:51:48 -0700

    Added automatic PKG_CONFIG_PATH attempt.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 setup(
     name='runcython',
-    version='0.2.4',
+    version='0.2.5',
     description='compile and run cython in one line',
     license='MIT',
     url='https://github.com/Russell91/runcython',
